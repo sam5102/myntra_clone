2704301cfb15266551a1d5343f8270b37c0e110d
sam5102 <ansh102u@gmail.com>
2022-11-21 23:36:37 +0530

    corrections added

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const toggleTheme = () => {
     const body = document.body;
     const nav = document.getElementById("nav_bar")
     const a_tag = document.querySelectorAll("[id='a_tag']")
-    const icons = document.getElementById("nav-icon")
     
+    nav.classList.toggle('dark-mode')
     
-    nav.classList.toggle('bg-dark')
-    icons.classList.toggle("dark-mode")
     for (let i = 0; i < a_tag.length; i++) {
         const element = a_tag[i];
+        console.log(element);
+        element.classList.toggle('dark-mode')
+    }
+
+    const icons = document.querySelectorAll("[id='nav_icon']")
+        for (let i = 0; i < icons.length; i++) {
+        const element = icons[i];
         console.log(element);
         element.classList.toggle('dark-mode')
     }
